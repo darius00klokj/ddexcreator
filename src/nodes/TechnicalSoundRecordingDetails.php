cf86775e1da6698fb034376d46eed794da9ef8f9
Darius Karremans <darius00klokj@gmail.com>
2021-07-14 15:44:12 +0200

    Adding nodes

--- src/nodes/TechnicalSoundRecordingDetails.php
+++ src/nodes/TechnicalSoundRecordingDetails.php
@@ -39,4 +39,81 @@ class TechnicalSoundRecordingDetails extends \DDEX\DDEXNode {
     public $BitsPerSample;
     
     public $Duration;
+    
+    /**
+     * 
+     * @var File
+     */
+    public $File;
+    
+    function __construct($TechnicalResourceDetailsReference, $AudioCodecType) {
+        $this->TechnicalResourceDetailsReference = $TechnicalResourceDetailsReference;
+        $this->AudioCodecType = $AudioCodecType;
+    }
+
+    
+    function getTechnicalResourceDetailsReference() {
+        return $this->TechnicalResourceDetailsReference;
+    }
+
+    function getAudioCodecType() {
+        return $this->AudioCodecType;
+    }
+
+    function getBitRate(): \DDEX\DDEXSingleNode {
+        return $this->BitRate;
+    }
+
+    function getNumberOfChannels() {
+        return $this->NumberOfChannels;
+    }
+
+    function getSamplingRate(): \DDEX\DDEXSingleNode {
+        return $this->SamplingRate;
+    }
+
+    function getBitsPerSample() {
+        return $this->BitsPerSample;
+    }
+
+    function getDuration() {
+        return $this->Duration;
+    }
+
+    function setTechnicalResourceDetailsReference($TechnicalResourceDetailsReference): void {
+        $this->TechnicalResourceDetailsReference = $TechnicalResourceDetailsReference;
+    }
+
+    function setAudioCodecType($AudioCodecType): void {
+        $this->AudioCodecType = $AudioCodecType;
+    }
+
+    function setBitRate(\DDEX\DDEXSingleNode $BitRate): void {
+        $this->BitRate = $BitRate;
+    }
+
+    function setNumberOfChannels($NumberOfChannels): void {
+        $this->NumberOfChannels = $NumberOfChannels;
+    }
+
+    function setSamplingRate(\DDEX\DDEXSingleNode $SamplingRate): void {
+        $this->SamplingRate = $SamplingRate;
+    }
+
+    function setBitsPerSample($BitsPerSample): void {
+        $this->BitsPerSample = $BitsPerSample;
+    }
+
+    function setDuration($Duration): void {
+        $this->Duration = $Duration;
+    }
+    
+    function getFile(): File {
+        return $this->File;
+    }
+
+    function setFile(File $File): void {
+        $this->File = $File;
+    }
+
 }
